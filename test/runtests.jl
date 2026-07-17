7e4a910e9d9220bd6fa61c821cbdc64c84a263e6
Kristoffer Carlsson <kcarlsson89@gmail.com>
2026-07-17 21:53:04 +0200

    Raise minimum Julia version to 1.10

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,19 @@ using TerminalRegressionTests
 using Test
 import REPL
 
-@static if VERSION >= v"1.5"
-    @testset "EmulatedTerminal" begin
-        emuterm = TerminalRegressionTests.EmulatedTerminal()
-        if isdefined(REPL.LineEdit, :hascolor)
-            @test REPL.LineEdit.hascolor(emuterm)
-        end
-        peek_task = @async peek(emuterm)
-        wait(emuterm)
-        @test emuterm.waiting
-        print(emuterm.input_buffer, 'x')
-        TerminalRegressionTests.notify_condition(emuterm.filled)
-        @test fetch(peek_task) == UInt8('x')
-        @test read(emuterm, Char) == 'x'
-        finalize(emuterm.pty)
+@testset "EmulatedTerminal" begin
+    emuterm = TerminalRegressionTests.EmulatedTerminal()
+    if isdefined(REPL.LineEdit, :hascolor)
+        @test REPL.LineEdit.hascolor(emuterm)
     end
+    peek_task = @async peek(emuterm)
+    wait(emuterm)
+    @test emuterm.waiting
+    print(emuterm.input_buffer, 'x')
+    TerminalRegressionTests.notify_condition(emuterm.filled)
+    @test fetch(peek_task) == UInt8('x')
+    @test read(emuterm, Char) == 'x'
+    finalize(emuterm.pty)
 end
 
 TerminalRegressionTests.automated_test(
